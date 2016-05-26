5736141424a486d4f9f711084bc114736f3095d3
jackness <jackness1208@qq.com>
2016-05-26 20:13:36 +0800

    [UPD] test

--- gulpfile.js
+++ gulpfile.js
@@ -39,34 +39,45 @@ gulp.task('start', function(){
 });
 
 gulp.task('pack', function(){
+    // return gulp.src('**')
+    //     .pipe(electron({ version: '0.34.1', platform: 'darwin' }))
+    //     .pipe(zip.dest('app-darwin.zip'));
+
     var 
         system = process.argv[4],
+        path = require('path'),
         pkg = require('./package.json'),
-        iconPath = '';
+        iconPath = '',
+        opts = {
+            version: '0.30.3',
+            winIcon: path.join(__dirname, 'app/icon/app-icon.ico'),
+            // darwinIcon: './app/icon/app-icon.icns',
+            platform: 'darwin'
+        };
 
     switch(system){
         case 'windows':
-            iconPath = './app/icon/app-icon.ico';
+        case 'pc':
+            opts.platform = 'win32';
             break;
 
         case 'mac':
-            iconPath = './app/icon/app-icon.icns';
+            opts.platform = 'darwin';
             break;
+
+        case 'linux':
+            opts.platform = 'linux';
+            break;
+        default:
+            return;
     }
 
+    return gulp.src(['**', '!build/**'])
+        .pipe(electron(opts))
+        .pipe(zip.dest('./build/'+ pkg.name +'.'+ pkg.version + '.' + opts.platform + '.zip'));
 
-    // var 
-    //     cmdStr = [
-    //         'electron-packager ./',
-    //         pkg.name,
-    //         '--all',
-    //         '--out ~/Desktop/'+ pkg.name,
-    //         '--version '+ pkg.version,
-    //         '--overwrite',
-    //         '--icon=' + iconPath
-    //     ].join(' ');
+    // return gulp.src('**')
+    //     .pipe(electron(opts))
+    //     .pipe(zip.dest('./build/'+ pkg.name +'.'+ pkg.version +'.zip'));
 
-    // fn.runCMD(cmdStr, function(){
-    //     console.log(['==============', 'done'].join('\n'));
-    // }, __dirname);
 });
